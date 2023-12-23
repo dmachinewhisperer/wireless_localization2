aed76fab64ecd5c201ba86019a54cdcdc318945d
Asogwa Emmanuel <asogwaemmanuel36@gmail.com>
2023-12-23 22:09:10 +0530

    rm pausing/resuming threads

--- calibration/websocket_client/ws_client.py
+++ calibration/websocket_client/ws_client.py
@@ -12,23 +12,16 @@ warnings.filterwarnings("ignore")
 # Lock to control thread pausing and resuming
 thread_lock = _thread.allocate_lock()
 pause_thread = False
+exception = None; 
 
+wss_response = None
 
 def on_message(ws, message):
+    global wss_response
 
     print(f"Received message: {message}")
-
-    db = "devdb.db"
-    connection = sqlite3.connect(db)
-    process_aps.process_wss_res(message)
-    process_aps.update_fv(connection)
-    process_aps.construct_fingerprint(connection)
-
-    connection.close()
-
-    global pause_thread
-    pause_thread = True
-
+    wss_response = message; 
+   
 def on_error(ws, error):
     print(f"Error occurred: {error}")
 
@@ -42,14 +35,20 @@ def on_open(ws):
     _thread.start_new_thread(send_user_input, (ws,))
 
 def send_user_input(ws):
+    global wss_response
+    
     global pause_thread
     while True:
-        with thread_lock:
-            if pause_thread:
-                pause_thread = False
-                _thread.exit() 
+        if wss_response is not None:
+            db = "devdb.db"
+            connection = sqlite3.connect(db)
+            process_aps.process_wss_res(wss_response)
+            process_aps.update_fv(connection)
+            process_aps.construct_fingerprint(connection)
+            connection.close()
+            wss_response = None
 
-        user_input = input("Enter message to send (or type 'exit' to close the connection): ")
+        user_input = input("Enter message to send (or type 'exit' quit): ")
         if user_input.lower() == 'exit':
             ws.close()
             break
@@ -60,7 +59,10 @@ def send_user_input(ws):
 
 if __name__ == "__main__":
     websocket.enableTrace(False)
-    esp32_ipv4_ip_addr = input("Enter WSS addr: ")
+    esp32_ipv4_ip_addr = ''
+    while esp32_ipv4_ip_addr =='':
+        esp32_ipv4_ip_addr = input("Enter WSS addr: ")
+
     ws = websocket.WebSocketApp('ws://' + esp32_ipv4_ip_addr + ':80/ws',
                                 on_open=on_open,
                                 on_message=on_message,
