f1ee35cfb8707fa8c93491270cabb8d634343b44
Asogwa Emmanuel <asogwaemmanuel36@gmail.com>
2024-01-15 18:30:13 +0530

    add pi online ws server

--- server/ws_server.py
+++ server/ws_server.py
@@ -1,12 +1,35 @@
+import joblib
 import socket
 import asyncio
 import websockets
+import process_aps_online
 
 async def server_handler(websocket, path):
+    global knn_loc_algorithm
+
+    db = "devdb.db"
+    connection = sqlite3.connect(db)
     while True:
         try:
             message = await websocket.recv()
             print(f"Received message: {message}")
+
+            process_aps_online.process_wss_res(wss_response)
+            fingerprint = process_aps_online.construct_fingerprint(connection)
+
+            #Guard against target being Out Of Range (OOF)
+            non_zero = 0
+            len_fingeprint = len(fingeprint)
+            for j in range(len(len_fingeprint)):
+                if fingeprint[j] != 0:
+                    non_zero = non_zero + 1
+            #############################################
+
+            
+            if non_zero < (0.1 * len_fingeprint):
+                message = "OOF"
+            else:
+                message = knn_loc_algorithm.predict(fingerprint)
 
             await websocket.send(message)
             print(f"Sent message back: {message}")
@@ -15,6 +38,8 @@ async def server_handler(websocket, path):
             break
 
 if __name__ == "__main__":
+    #load model
+    knn_loc_algorithm = joblib.load('knn_loc_algorithm.sav')
     # Get IP address of machine
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
