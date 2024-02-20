60c54dbf60ee74ed4285c998f96e5c14b1b53843
Asogwa Emmanuel <asogwaemmanuel36@gmail.com>
2024-02-20 10:37:41 +0530

    cleaned up main

--- device/websocket_client(ESP32)/main/main.c
+++ device/websocket_client(ESP32)/main/main.c
@@ -10,31 +10,65 @@
 #include "protocol_examples_common.h"
 
 #include "ws_client.h"
-
 #include "mqtt_client1.h"
 
-static const char *TAG = "main";
+#include "init.h"
+
+static const char *TAG = "MAIN";
+
 
 void app_main(void)
 {
-    //static httpd_handle_t server = NULL;
+    /* Semaphore for synchronising read and load endpoint processes
+    */
+    xSemaphore = xSemaphoreCreateBinary();
 
     ESP_LOGI(TAG, "[APP] Startup..");
     ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
+
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+  
+    /*set device public idenfier
+     *this is the name the outside device identies this device
+     *also publishes location data to this topic
+     *to avoid conflict with other devices, a truly univerally unique id is required
+     *this function sets it to the BASE MAC ADDRESS
+     */
+    set_device_public_id();
+
+    /*logic to read-in tracked node(s) ids
+     *if(get_line())
+     */
+
+    /* attempt to get endpoints of broker and server from stdin and write to nvs
+     * if the user does not input them after 1 min, TIMEOUT ocurrs
+     * attempt to read previous ones stored in nvs
+     * both tasks self -delete after completing
+     */
+    xTaskCreate(task_get_endpoints, "get_uri1", configMINIMAL_STACK_SIZE + 1024, NULL, tskIDLE_PRIORITY+1, &task_get_endpoints_xhandle);
+    xTaskCreate(task_load_endpoints, "get_uri2", configMINIMAL_STACK_SIZE + 1024, NULL, tskIDLE_PRIORITY +1 , NULL);
+
+    //debug lines 
+    //while(1);
+
+    /*initialize output 
+     *i2c display used -> 0.91 OLED display with SSD1309 controller
+     */
+    //init_display();
 
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
      */
-    ESP_ERROR_CHECK(example_connect());
+    //ESP_ERROR_CHECK(example_connect());
 
-    /* Start the clients for the first time */
-    /*mqtt client must be started before ws client*/
-    start_mqtt_client();
-    start_websocket_client();
+    /* Start the clients for the first time 
+     * mqtt client must be started before ws client
+     */
+    //start_mqtt_client(mqtt_broker_endpoint);
+    //start_websocket_client(ws_server_endpoint);
 }
